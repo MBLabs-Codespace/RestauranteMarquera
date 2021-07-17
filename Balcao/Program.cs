9fc2d17a8934c64e6e4c7f1c2212bfb938b1b581
MarcosVasconcellosJr <marcosjr.vcs@gmail.com>
2021-07-17 12:04:43 -0300

    websockets

---
+++ Balcao/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace Balcao
+{
+    public class Program
+    {
+        public class Order
+        {
+            public string Symbol { get; set; }
+            public decimal Price { get; set; }
+        }
+        
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            using (var hubConnection = new HubConnection("http://www.contoso.com/")) 
+            {
+                IHubProxy stockTickerHubProxy = hubConnection.CreateHubProxy("StockTickerHub");
+                stockTickerHubProxy.On<Stock>("OrderCreated", stock =>
+                    Console.WriteLine("Stock update for {0} new price {1}", stock.Symbol, stock.Price));
+                
+                await hubConnection.Start();
+            }
+        }
+    }
+}
--- Restaurante/Startup.cs
+++ Restaurante/Startup.cs
@@ -1,5 +1,11 @@
+using System;
+using System.Net;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -38,13 +44,52 @@ namespace Restaurante
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Restaurante v1"));
             }
 
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/ws")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        using (WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync())
+                        {
+                            await Echo(context, webSocket);
+                        }
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = (int) HttpStatusCode.BadRequest;
+                    }
+                }
+                else
+                {
+                    await next();
+                }
+
+            });
+
+            
             app.UseHttpsRedirection();
 
+            app.UseWebSockets();
+            
             app.UseRouting();
 
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+        
+        private async Task Echo(HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
         }
     }
 }
